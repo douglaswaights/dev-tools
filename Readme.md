718d37f63b445e7e06c9b8574e37d0f8fed26e4d
dwaights <dwaights>
2022-10-14 11:08:53 +0200

    updates

--- Readme.md
+++ Readme.md
@@ -1,12 +1,12 @@
 ## Dev Tools helpers and notes
 
 # git
- git config --global user.email "you@example.com"
- git config --global user.name "Your Name"
+git config --global user.email "dwaights"
+git config --global user.name "dwaights"
 
 # ssh
 ssh-keygen to create ssh keys on local machine
 
 # run bash script to install common dev tools
 
-curl -Lo run.sh <url>
+curl -s https://github.com/douglaswaights/dev-tools/install-tools.sh | bash
---
+++ install-tools.sh
@@ -0,0 +1,20 @@
+#bin/bash
+
+#install lazy git
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"v*([^"]+)".*/\1/')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
+
+# k8s (assume docker present)
+
+#k3d
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+#kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/bin/
+#k9s
+curl -Lo k9s.tar.gz "https://github.com/derailed/k9s/releases/download/v0.26.6/k9s_Linux_x86_64.tar.gz"
+sudo tar xf k9s.tar.gz -C /usr/local/bin k9s
+
+
